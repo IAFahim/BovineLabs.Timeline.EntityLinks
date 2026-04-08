a0f1394a7c0c6736e80c8c49de033c77944a7eff
IAFahim <ishtiaq.fahim@northsouth.edu>
2026-04-08 15:26:39 +0600

    init

---
+++ Bovinelabs.Timeline.Entity.Links.Authoring/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Runtime.CompilerServices;
+using Unity.Entities;
+
+[assembly: DisableAutoTypeRegistration]
+
+[assembly: InternalsVisibleTo("Bovinelabs.Timeline.Entity.Links.Editor")]
+[assembly: InternalsVisibleTo("Bovinelabs.Timeline.Entity.Links.Tests")]
---
+++ Bovinelabs.Timeline.Entity.Links/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Bovinelabs.Timeline.Entity.Links.Debug")]
+[assembly: InternalsVisibleTo("Bovinelabs.Timeline.Entity.Links.Editor")]
+[assembly: InternalsVisibleTo("Bovinelabs.Timeline.Entity.Links.Tests")]
